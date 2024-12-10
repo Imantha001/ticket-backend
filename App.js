d89019bfebb7bf71271ab8afa062ca1ce47418d7
L. A. W. Imantha Sankalpa <imanthasankalpa@gmail.com>
2024-12-10 13:16:13 +0530

    Mongodb database connect added

--- App.js
+++ App.js
@@ -1,28 +1,44 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
-
+const port = process.env.PORT || 2001;
 
 app.use(helmet());
 app.use(cors());
-app.use(morgan("tiny"));
+
+// MongoDB Connection Setup
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // useCreateIndex: true,
+});
+if (process.env.NODE_ENV !== "production") {
+  const mDb = mongoose.connection;
+  mDb.on("open", () => {
+    console.log("MongoDB is connected");
+  });
+  mDb.on("error", (error) => {
+    console.log(error);
+  });
+
+  // Logger
+  app.use(morgan("tiny"));
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const port = process.env.PORT || 2001;
 
 const userRouter = require("./src/routers/userRouter");
 const ticketRouter = require("./src/routers/ticketRouter");
 
 app.use("/u1/user", userRouter);
 app.use("/u1/ticket", ticketRouter);
-
-
-
 
 const handleError = require("./src/Utills/errorHandling");
 app.use((req, res, next) => {
@@ -34,8 +50,6 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   handleError(error, res);
 });
-
-
 
 app.listen(port, () => {
   console.log(`API is ready on http://localhost:${port}`);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const { route } = require("./ticketRouter");
+
+const { insertUser } = require("../model/users/user.model");
+const { hashPassword } = require("../helpers/bcrypt.helper");
+
 
 router.all("/", (req, res, next) => {
-  console.log(user);
-  res.json({ message: "from user router" });
+  // res.json({ message: "from user router" });
+  next();
 });
 
 
-
+router.post("/", async (req, res) => {
+  const { name, company, address, phone, email, password } = req.body;
+  try {
+    //hash password
+    const hashedPass = await hashPassword(password);
+    const newUserObj = {
+      name,
+      company,
+      address,
+      phone,
+      email,
+      password: hashedPass,
+    };
+    const result = await insertUser(newUserObj);
+    console.log(result);
+    res.json({ message: "New user created", result });
+  } catch (error) {
+    console.log(error);
+    res.json({ statux: "error", message: error.message });
+  }
+});
 module.exports = router;
